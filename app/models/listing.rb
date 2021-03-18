eb4c7338fb6bb8b75bef266d7b7d86b7e0a5d263
Shilcof <robert.shilcof@gmail.com>
2021-03-18 14:48:52 +0000

    adds length validators to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,7 @@ class Listing < ApplicationRecord
 
   validates :name, presence: true, no_symbols: true
   validates :description, presence: true, no_symbols: true
+
+  validates :name, length: { in: 3..30 }, allow_blank: true
+  validates :description, length: { in: 50..2000 }, allow_blank: true
 end
