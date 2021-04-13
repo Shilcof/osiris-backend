1c79f31ddce4a83c2ca124276e5b400238a161a4
Shilcof <robert.shilcof@gmail.com>
2021-04-13 12:56:27 +0100

    migrates database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_103340) do
+ActiveRecord::Schema.define(version: 2021_04_13_110618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_19_103340) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.index ["seller_id"], name: "index_listings_on_seller_id"
   end
 
