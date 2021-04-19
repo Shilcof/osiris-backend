e12b6d2089825f2f5ba5cfb0166d17a5a169b487
Shilcof <robert.shilcof@gmail.com>
2021-04-19 16:31:40 +0100

    adds randomised latitude and longitude to seeded listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,7 +61,9 @@ def make_listings
         20.times {
             path = paths.sample
             s.listings.create({
-                name: [LISTING_START.sample, LISTING_END.sample].join(" "), 
+                name: [LISTING_START.sample, LISTING_END.sample].join(" "),
+                latitude: AVERAGE_LATLNG["lat"] + rand()*50 - 25,
+                longitude: AVERAGE_LATLNG["lng"] + rand()*50 - 25,
                 description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
             }).image.attach(io: File.open(File.expand_path(path)), filename: "#{path}.jpg")
         }
